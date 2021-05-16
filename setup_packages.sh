f82e291a886a4fa04f3e447c99154061b6064cbf
Kjetil Iversen <kjeivers@gmail.com>
2021-05-16 10:49:05 +0200

    Adding trace of dependencies

---
+++ setup_packages.sh
@@ -0,0 +1,13 @@
+# run after setting up python3 virtual environment
+# python3 -m venv venv
+
+pip install flask
+pip install python-dotenv
+pip install flask-wtf
+pip install flask-sqlalchemy
+pip install flask-migrate
+pip install flask-login
+pip install email-validator
+
+# Possible to run `pip freeze > requirements.txt` afterwards
+# and then `pip install -r requirements.txt`
